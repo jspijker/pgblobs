6173f6244ce1b491bedf38e1c8785119d50ef7f0
Job Spijker <job.spijker@rivm.nl>
2019-03-14 12:21:59 +0100

    added importBlob function

---
+++ R/importBlob.R
@@ -0,0 +1,75 @@
+#' Import blobs 
+#'
+#' This function import blobs (file and meta-data file) from elsewhere
+#' into the current pbobjects database
+#'
+#' For each blobfile a meta-data file must exists in the same
+#' directory (with extention .ini)
+#'
+#' @return the name of the created blob object
+#'
+#' @param importfile full path of the file to import
+#' @param overwrite Should existing blobs be overwritten
+#'
+#' @export
+
+
+importBlob <- function(importfile,overwrite=FALSE) {
+
+    inifile <- paste(importfile,"ini",sep=".")
+
+    if(!file.exists(importfile)) {
+        stop(paste("importfile",importfile,"does not exists"))
+    }
+
+    # read ini file
+    if(!file.exists(inifile)) {
+        stop(paste("inifile",inifile,"does not exists"))
+    }
+
+    ini <- read.ini(inifile)
+
+    #copy file
+    destfile <- datafile(basename(importfile))
+    file.copy(importfile,destfile)
+
+    # extract meta data from ini file
+    fname <- subset(ini$kv,section=="meta"&key=="src")$value
+    name <- subset(ini$kv,section=="meta"&key=="name")$value
+    description <- subset(ini$kv,section=="meta"&key=="description")$value
+    md5 <- subset(ini$kv,section=="object"&key=="md5")$value
+
+    # create txt file with long description of blob
+    txtfile <- datafile("importblob_description.txt")
+    f <- file(txtfile)
+    cat(ini$text,file=f,sep="\n")
+    close(f)
+
+    # create kvlist
+    kvpairs <- subset(ini$kv,select=c("key","value"),section=="object")
+    kvlist <- kvpairs$value
+    names(kvlist) <- kvpairs$key
+    kvlist <- as.list(kvlist)
+
+
+    if(overwrite) {
+        if(objectExists(name)) {
+            deleteBlob(name)
+        }
+    }
+
+
+    x<-createBlob(fname=destfile,name=name,kv=kvlist,
+                  description=description,md5=md5,
+                  textfile=txtfile)
+
+
+    # # remove tmp file
+    file.remove(txtfile)
+    # # return name of object
+    invisible(name)
+    # 
+
+}
+
+
